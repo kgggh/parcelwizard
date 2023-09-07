9248b0b7e17703db24a5cb11e3b956749384352c
kgggh <68227318+kgggh@users.noreply.github.com>
2023-09-07 21:48:19 +0900

    project init

---
+++ src/main/java/com/gnnny/parcelwizard/ParcelwizardApplication.java
@@ -0,0 +1,13 @@
+package com.gnnny.parcelwizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcelwizardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParcelwizardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gnnny/parcelwizard/ParcelwizardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnnny.parcelwizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcelwizardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
