de243ea6b7f2a73700c9229485290599d587c855
kgggh <68227318+kgggh@users.noreply.github.com>
2023-10-17 01:45:22 +0900

    feat: react-web application 추가

---
+++ frontend/parcel-wizard/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+
+import Courier from './pages/Courier';
+import CustomsClearance from './pages/CustomsClearance';
+import Home from './pages/Home';
+import headerLogo from './assets/header-icon.png';
+
+const NavigationBar = () => {
+  return (
+    <nav className="nav">
+      <Link to="/clearance-clearances">통관조회</Link> |{' '}
+      <Link to="/couriers">배송조회</Link>
+    </nav>
+  );
+};
+
+const App = () => {
+  const navigate = useNavigate();
+
+  const handleImageClick = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className="App">
+      <div id="home-header">
+        <img
+          id="header-logo"
+          alt="header-logo"
+          src={headerLogo}
+          onClick={handleImageClick}
+        />
+      </div>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/couriers" element={<Courier />} />
+        <Route path="/clearance-clearances" element={<CustomsClearance />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/parcel-wizard/src/pages/Courier.js
@@ -0,0 +1,126 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Courier = () => {
+  const [trackingResult, setTrackingResult] = useState(null);
+
+  const [inputs, setInputs] = useState({
+    courierCompany: 'KOREA_POST',
+    trackingNo: '',
+  });
+
+  const { courierCompany, trackingNo } = inputs;
+
+  const onChangeValue = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const CourierTrackingDetail = () => {
+    return (
+      <div className="tracking-result">
+        <div>
+          <h2>기본정보</h2>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>운송장번호</th>
+              <th>보내는 분</th>
+              <th>받는 분</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{trackingResult.trackingNo}</td>
+              <td>{trackingResult.senderName}</td>
+              <td>{trackingResult.recipientName}</td>
+            </tr>
+          </tbody>
+        </table>
+        <div>
+          <h2>진행상황</h2>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>처리일시</th>
+              <th>위치</th>
+              <th>상태</th>
+            </tr>
+          </thead>
+          <tbody>
+            {trackingResult.shipmentProgressTrackingResources.map(
+              (shipmentProgress) => {
+                return (
+                  <tr key={shipmentProgress.processingDateTime}>
+                    <td>{shipmentProgress.processingDateTime}</td>
+                    <td>{shipmentProgress.location}</td>
+                    <td>{shipmentProgress.detailStatus}</td>
+                  </tr>
+                );
+              }
+            )}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const params = {
+      trackingNo: trackingNo,
+      courierCompany: courierCompany,
+    };
+
+    axios
+      .get('http://localhost:8081/api/v1/shipments/tracking', { params })
+      .then((resposne) => {
+        setTrackingResult(resposne.data);
+      })
+      .catch((e) => {
+        setTrackingResult(null);
+        alert(e.response.data.reason);
+      });
+  };
+
+  return (
+    <div className="courier-print">
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          <select
+            id="courier-company"
+            name="courierCompany"
+            onChange={onChangeValue}
+          >
+            <option value="KOREA_POST">우체국택배</option>
+            <option value="CJ_LOGISTICS">CJ대한통운</option>
+            <option value="WINION_LOGIS">위니온로지스</option>
+          </select>
+        </label>
+        <label>
+          <input
+            type="text"
+            id="tracking-no"
+            name="trackingNo"
+            onChange={onChangeValue}
+            placeholder="운송장 번호를 입력해주세요."
+          />
+        </label>
+        <button type="submit" id="search-button">
+          조회
+        </button>
+      </form>
+      {trackingResult && <CourierTrackingDetail />}
+    </div>
+  );
+};
+
+export default Courier;
